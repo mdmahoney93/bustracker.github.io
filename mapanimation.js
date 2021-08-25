25c708501ea111139ff68ae7878d0fbb9f4880d1
mdmahoney93 <83600327+mdmahoney93@users.noreply.github.com>
2021-08-24 20:28:08 -0500

    Update mapanimation.js

--- mapanimation.js
+++ mapanimation.js
@@ -18,7 +18,7 @@ mapboxgl.accessToken =
 
 let map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/mapbox/streets-v11",
+  style: "mapbox://styles/mapbox/navigation-night-v1",
   center: [-71.104081, 42.365554],
   zoom: 14,
 });
